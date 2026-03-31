fa9183bc72e989282965b5061f9f379c69bed109
jcbecleo <ecleojosscharyborj@gmail.com>
2026-03-31 22:22:22 +0800

    adjusted model to use gemini flash 2.5

--- app/lib/gemini.ts
+++ app/lib/gemini.ts
@@ -2,4 +2,4 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
-export const gemini = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+export const gemini = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
